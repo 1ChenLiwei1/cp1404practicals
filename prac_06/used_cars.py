1673fa3f038a7e7da562e83ccbd589d92a93f8a4
Liwei Chen 01 <liwei.chen1@my.jcu.edu.au>
2024-07-14 17:46:37 +0800

    submit job

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,9 +9,16 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
+    # my_car = Car(180)
+    # my_car.drive(30)
+    # print(f"Car has fuel: {my_car.fuel}")
+    # print(my_car)
+    my_car = Car(name='My car', fuel=50)
+    limo = Car(name='Limo', fuel=100)
+    limo.add_fuel(20)
+    print(f'Fuel in limo: {limo.fuel}')
+    limo.drive(115)
+    print(limo)
     print(my_car)
 
 
